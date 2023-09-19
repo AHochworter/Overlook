36506f9b7eb117dafa35c9d30b7c3927dd668c2e
Ann Hochworter <ann.hochworter@gmail.com>
2023-09-19 14:11:41 -0600

    Start skeleton html

--- sample-data/sample-bookings.js
+++ sample-data/sample-bookings.js
@@ -1,140 +1,278 @@
-"bookings" = [
-    {
-    "id": "5fwrgu4i7k55hl6sz",
-    "userID": 39,
-    "date": "2022/04/22",
-    "roomNumber": 2
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t5",
-    "userID": 41,
-    "date": "2022/01/24",
-    "roomNumber": 4
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t6",
-    "userID": 49,
-    "date": "2022/01/10",
-    "roomNumber": 5
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t7",
-    "userID": 20,
-    "date": "2022/02/16",
-    "roomNumber": 7
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t8",
-    "userID": 39,
-    "date": "2022/02/05",
-    "roomNumber": 8
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t9",
-    "userID": 20,
-    "date": "2023/12/14",
-    "roomNumber": 9
-    },
-    {
-    "id": "5fwrgu4i7k55hl6ta",
-    "userID": 25,
-    "date": "2022/01/11",
-    "roomNumber": 9
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tb",
-    "userID": 22,
-    "date": "2022/02/06",
-    "roomNumber": 7
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tc",
-    "userID": 43,
-    "date": "2023/11/30",
-    "roomNumber": 8
-    },
-    {
-    "id": "5fwrgu4i7k55hl6td",
-    "userID": 22,
-    "date": "2022/01/31",
-    "roomNumber": 7
-    },
-    {
-    "id": "5fwrgu4i7k55hl6te",
-    "userID": 27,
-    "date": "2022/01/19",
-    "roomNumber": 4
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tf",
-    "userID": 47,
-    "date": "2022/01/25",
-    "roomNumber": 5,
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tg",
-    "userID": 25,
-    "date": "2022/02/03",
-    "roomNumber": 1,
-    },
-    {
-    "id": "5fwrgu4i7k55hl6th",
-    "userID": 38,
-    "date": "2022/02/26",
-    "roomNumber": 1,
-    },
-    {
-    "id": "5fwrgu4i7k55hl6ti",
-    "userID": 39,
-    "date": "2022/01/22",
-    "roomNumber": 8
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tj",
-    "userID": 36,
-    "date": "2022/01/17",
-    "roomNumber": 5
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tk",
-    "userID": 41,
-    "date": "2023/11/27",
-    "roomNumber": 4
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tl",
-    "userID": 27,
-    "date": "2022/01/10",
-    "roomNumber": 5
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tm",
-    "userID": 48,
-    "date": "2022/01/16",
-    "roomNumber": 3
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tn",
-    "userID": 20,
-    "date": "2022/01/17",
-    "roomNumber": 10
-    },
-    {
-    "id": "5fwrgu4i7k55hl6to",
-    "userID": 41,
-    "date": "2022/02/22",
-    "roomNumber": 10
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tp",
-    "userID": 43,
-    "date": "2023/11/23",
-    "roomNumber": 10
-    },
-    {
-    "id": "5fwrgu4i7k55hl6tq",
-    "userID": 49,
-    "date": "2022/02/03",
-    "roomNumber": 10
-    },
-]
+[
+  {
+    id: 'Sample_5fwrgu4i7k55hl6sz',
+    userID: 1,
+    date: '1/8/23',
+    roomNumber: 8,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6t5',
+    userID: 1,
+    date: '1/10/23',
+    roomNumber: 12,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6t6',
+    userID: 1,
+    date: '1/25/23',
+    roomNumber: 2,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6t7',
+    userID: 1,
+    date: '1/26/23',
+    roomNumber: 20,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6t8',
+    userID: 2,
+    date: '1/19/23',
+    roomNumber: 5,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6t9',
+    userID: 2,
+    date: '1/20/23',
+    roomNumber: 24,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6ta',
+    userID: 2,
+    date: '1/24/23',
+    roomNumber: 24,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tb',
+    userID: 2,
+    date: '1/25/23',
+    roomNumber: 9,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tc',
+    userID: 2,
+    date: '1/31/23',
+    roomNumber: 20,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6td',
+    userID: 2,
+    date: '2/1/23',
+    roomNumber: 11,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6te',
+    userID: 2,
+    date: '11/27/23',
+    roomNumber: 20,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tf',
+    userID: 2,
+    date: '11/28/23',
+    roomNumber: 22,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tg',
+    userID: 4,
+    date: '1/22/23',
+    roomNumber: 11,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6th',
+    userID: 4,
+    date: '1/23/23',
+    roomNumber: 5,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6ti',
+    userID: 4,
+    date: '11/23/23',
+    roomNumber: 22,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tj',
+    userID: 4,
+    date: '11/24/23',
+    roomNumber: 7,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tk',
+    userID: 5,
+    date: '1/7/23',
+    roomNumber: 12,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tl',
+    userID: 5,
+    date: '1/9/23',
+    roomNumber: 5,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tm',
+    userID: 5,
+    date: '1/11/23',
+    roomNumber: 9,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tn',
+    userID: 5,
+    date: '1/12/23',
+    roomNumber: 2,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6to',
+    userID: 5,
+    date: '12/14/23',
+    roomNumber: 14,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tp',
+    userID: 5,
+    date: '12/15/23',
+    roomNumber: 8,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6to',
+    userID: 6,
+    date: '1/19/23',
+    roomNumber: 8,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tq',
+    userID: 6,
+    date: '1/20/23',
+    roomNumber: 7,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tr',
+    userID: 6,
+    date: '2/3/23',
+    roomNumber: 17,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6sz',
+    userID: 6,
+    date: '2/6/23',
+    roomNumber: 8,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6ts',
+    userID: 6,
+    date: '2/7/23',
+    roomNumber: 5,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tt',
+    userID: 6,
+    date: '2/8/23',
+    roomNumber: 17,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tu',
+    userID: 6,
+    date: '2/16/23',
+    roomNumber: 7,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tv',
+    userID: 6,
+    date: '2/17/23',
+    roomNumber: 13,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tw',
+    userID: 6,
+    date: '3/26/23',
+    roomNumber: 15,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tx',
+    userID: 6,
+    date: '12/14/23',
+    roomNumber: 19,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6ty',
+    userID: 7,
+    date: '4/22/23',
+    roomNumber: 15,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6sz',
+    userID: 7,
+    date: '4/23/23',
+    roomNumber: 14,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6sz',
+    userID: 8,
+    date: '2/4/23',
+    roomNumber: 12,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6tz',
+    userID: 8,
+    date: '2/5/23',
+    roomNumber: 12,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u0',
+    userID: 8,
+    date: '2/5/23',
+    roomNumber: 12,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u1',
+    userID: 8,
+    date: '2/6/23',
+    roomNumber: 20,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u2',
+    userID: 8,
+    date: '2/22/23',
+    roomNumber: 13,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u3',
+    userID: 8,
+    date: '12/14/23',
+    roomNumber: 12,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u4',
+    userID: 10,
+    date: '1/16/23',
+    roomNumber: 19,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u5',
+    userID: 10,
+    date: '1/17/23',
+    roomNumber: 15,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u6',
+    userID: 10,
+    date: '1/18/23',
+    roomNumber: 7,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u7',
+    userID: 10,
+    date: '1/21/23',
+    roomNumber: 13,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u8',
+    userID: 10,
+    date: '11/30/23',
+    roomNumber: 13,
+  },
+  {
+    id: 'Sample_5fwrgu4i7k55hl6u9',
+    userID: 10,
+    date: '12/1/23',
+    roomNumber: 15,
+  },
+];
